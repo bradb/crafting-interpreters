54ec8b13c6c190c853ae407fd24f61b31c7c1738
Brad Bollenbach <bradb@30sleeps.com>
2024-10-15 14:21:07 +0100

    tidying and test stuff

--- clj/test/lox/scanner_test.clj
+++ clj/test/lox/scanner_test.clj
@@ -2,44 +2,46 @@
   (:require  [clojure.test :refer [deftest is]]
              [lox.scanner :as s]))
 
+(def t s/->Token)
+
 (deftest single-char-tokens-test
-  (is (= [(s/->Token ::s/left-paren "(" nil 1)] (s/scan "(")))
-  (is (= [(s/->Token ::s/right-paren ")" nil 1)] (s/scan ")")))
-  (is (= [(s/->Token ::s/left-brace "{" nil 1)] (s/scan "{")))
-  (is (= [(s/->Token ::s/right-brace "}" nil 1)] (s/scan "}")))
-  (is (= [(s/->Token ::s/comma "," nil 1)] (s/scan ",")))
-  (is (= [(s/->Token ::s/dot "." nil 1)] (s/scan ".")))
-  (is (= [(s/->Token ::s/minus "-" nil 1)] (s/scan "-")))
-  (is (= [(s/->Token ::s/plus "+" nil 1)] (s/scan "+")))
-  (is (= [(s/->Token ::s/semicolon ";" nil 1)] (s/scan ";")))
-  (is (= [(s/->Token ::s/star "*" nil 1)] (s/scan "*"))))
+  (is (= [(t ::s/left-paren "(" nil 1)] (s/scan "(")))
+  (is (= [(t ::s/right-paren ")" nil 1)] (s/scan ")")))
+  (is (= [(t ::s/left-brace "{" nil 1)] (s/scan "{")))
+  (is (= [(t ::s/right-brace "}" nil 1)] (s/scan "}")))
+  (is (= [(t ::s/comma "," nil 1)] (s/scan ",")))
+  (is (= [(t ::s/dot "." nil 1)] (s/scan ".")))
+  (is (= [(t ::s/minus "-" nil 1)] (s/scan "-")))
+  (is (= [(t ::s/plus "+" nil 1)] (s/scan "+")))
+  (is (= [(t ::s/semicolon ";" nil 1)] (s/scan ";")))
+  (is (= [(t ::s/star "*" nil 1)] (s/scan "*"))))
 
 (deftest multiple-single-tokens-test
-  (is (= [(s/->Token ::s/left-paren "(" nil 1)
-          (s/->Token ::s/right-paren ")" nil 1)]
+  (is (= [(t ::s/left-paren "(" nil 1)
+          (t ::s/right-paren ")" nil 1)]
          (s/scan "()")))
-  (is (= [(s/->Token ::s/semicolon ";" nil 1)
-          (s/->Token ::s/dot "." nil 1)
-          (s/->Token ::s/star "*" nil 1)
-          (s/->Token ::s/minus "-" nil 1)]
+  (is (= [(t ::s/semicolon ";" nil 1)
+          (t ::s/dot "." nil 1)
+          (t ::s/star "*" nil 1)
+          (t ::s/minus "-" nil 1)]
          (s/scan ";.*-"))))
 
 (deftest one-and-two-character-lexemes-test
-  (is (= [(s/->Token ::s/bang "!" nil 1)]
+  (is (= [(t ::s/bang "!" nil 1)]
          (s/scan "!")))
-  (is (= [(s/->Token ::s/equal "=" nil 1)]
+  (is (= [(t ::s/equal "=" nil 1)]
          (s/scan "=")))
-  (is (= [(s/->Token ::s/less "<" nil 1)]
+  (is (= [(t ::s/less "<" nil 1)]
          (s/scan "<")))
-  (is (= [(s/->Token ::s/greater ">" nil 1)]
+  (is (= [(t ::s/greater ">" nil 1)]
          (s/scan ">")))
-  (is (= [(s/->Token ::s/bang-equal "!=" nil 1)]
+  (is (= [(t ::s/bang-equal "!=" nil 1)]
          (s/scan "!=")))
-  (is (= [(s/->Token ::s/equal-equal "==" nil 1)]
+  (is (= [(t ::s/equal-equal "==" nil 1)]
          (s/scan "==")))
-  (is (= [(s/->Token ::s/less-equal "<=" nil 1)]
+  (is (= [(t ::s/less-equal "<=" nil 1)]
          (s/scan "<=")))
-  (is (= [(s/->Token ::s/greater-equal ">=" nil 1)]
+  (is (= [(t ::s/greater-equal ">=" nil 1)]
          (s/scan ">="))))
 
 (deftest string-literal-test
